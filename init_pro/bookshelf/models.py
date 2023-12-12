4072eba6b3de34b30ca0a4133ecea01de294f379
Yossra Antari <yossraantari@yossras-macbook-air.home>
2023-12-12 12:18:51 +0100

    model: change database model

--- init_pro/bookshelf/models.py
+++ init_pro/bookshelf/models.py
@@ -2,8 +2,34 @@ from django.db import models
 
 # Create your models here.
 
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     publication_date = models.DateField()
-    genre = models.CharField(max_length=255)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=255)
+    comment = models.TextField()
+    rate = models.IntegerField()  # add constraints for the rating range
+
+    def __str__(self):
+        return f"Review for {self.book.name} by {self.user_name}"
